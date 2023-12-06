eb3dfdaa1453c7ddda9c8e2731a3784949d6c617
Brett Havekost <458427+bhavekost@users.noreply.github.com>
2023-12-05 22:32:15 -0600

    Inserted the initial text into the file. Will debug from here.

--- MyTest.ps1
+++ MyTest.ps1
@@ -1,8 +1,8 @@
 python.exe -m pip install --upgrade pip
 Start-Sleep -s 3
-pip freeze | %{$_.split('==')[0]} | %{pip install --upgrade $_}
+pip freeze | ForEach-Object{$_.split('==')[0]} | ForEach-Object{pip install --upgrade $_}
 Start-Sleep -s 3
-.\Update-AllPowerShellModules.ps1
+#.\Update-AllPowerShellModules.ps1
 Start-Sleep -s 3
 Import-Module PSWindowsUpdate
 Start-Sleep -s 3
@@ -16,12 +16,13 @@ Optimize-Volume -DriveLetter C,D,T -ReTrim -Verbose
 Start-Sleep -s 3
 Optimize-Volume -DriveLetter D,H -Defrag -Verbose
 Start-Sleep -s 3
-cd "C:\Program Files\Dell\CommandUpdate\"
-.\dcu-cli.exe /configure -silent -autoSuspendBitLocker=enable -userConsent=disable
-.\dcu-cli.exe /scan -outputLog=C:\dell\logs\scan.log
-.\dcu-cli.exe /applyUpdates -reboot=disable -outputLog=C:\dell\logs\applyUpdates.log 
-cd "C:\temp"
+#cd "C:\Program Files\Dell\CommandUpdate\"
+#.\dcu-cli.exe /configure -silent -autoSuspendBitLocker=enable -userConsent=disable
+#.\dcu-cli.exe /scan -outputLog=C:\dell\logs\scan.log
+#.\dcu-cli.exe /applyUpdates -reboot=disable -outputLog=C:\dell\logs\applyUpdates.log 
+#cd "C:\temp"
 Start-Sleep -s 3
 # start-MpScan -ScanType FullScan
 Start-Sleep -s 10
+#some text went here
 exit
