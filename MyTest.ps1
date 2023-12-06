570fd231d3c38129da63d0f238ae3a060fe8593e
Brett Havekost <brett.havekost@gmail.com>
2023-12-05 22:13:57 -0600

    The initial push of all update lines of code that I run weekly to keep Windows up and running.

---
+++ MyTest.ps1
@@ -0,0 +1,27 @@
+python.exe -m pip install --upgrade pip
+Start-Sleep -s 3
+pip freeze | %{$_.split('==')[0]} | %{pip install --upgrade $_}
+Start-Sleep -s 3
+.\Update-AllPowerShellModules.ps1
+Start-Sleep -s 3
+Import-Module PSWindowsUpdate
+Start-Sleep -s 3
+Reset-WUComponents -Verbose
+Start-Sleep -s 3
+Get-WindowsUpdate -AcceptAll -Install -Autoreboot
+Start-Sleep -s 3
+winget.exe upgrade --all
+Start-Sleep -s 3
+Optimize-Volume -DriveLetter C,D,T -ReTrim -Verbose
+Start-Sleep -s 3
+Optimize-Volume -DriveLetter D,H -Defrag -Verbose
+Start-Sleep -s 3
+cd "C:\Program Files\Dell\CommandUpdate\"
+.\dcu-cli.exe /configure -silent -autoSuspendBitLocker=enable -userConsent=disable
+.\dcu-cli.exe /scan -outputLog=C:\dell\logs\scan.log
+.\dcu-cli.exe /applyUpdates -reboot=disable -outputLog=C:\dell\logs\applyUpdates.log 
+cd "C:\temp"
+Start-Sleep -s 3
+# start-MpScan -ScanType FullScan
+Start-Sleep -s 10
+exit
